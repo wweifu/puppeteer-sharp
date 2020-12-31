e3a5ba5eda35ab6e3a3bce32ee252250b12f6adf
wweifu <125662872@qq.com>
2021-01-01 00:55:50 +0800

    在 FetchFulfillRequest  修复不能json序列化的bug  ReasonPhrases.GetReasonPhrase 是 .netCore 项目的 .net FrameWorker 4,用  new HttpResponseMessage((HttpStatusCode)_responseCode); 代替

--- lib/PuppeteerSharp/Messaging/FetchFulfillRequest.cs
+++ lib/PuppeteerSharp/Messaging/FetchFulfillRequest.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace PuppeteerSharp.Messaging
@@ -7,12 +9,24 @@ namespace PuppeteerSharp.Messaging
     {
         public string RequestId { get; set; }
 
-        public int ResponseCode { get; set; }
+        public int ResponseCode
+        {
+            get => _responseCode;
+            set
+            {
+                _responseCode = value;
+                var d = new HttpResponseMessage((HttpStatusCode)_responseCode);
+                ResponsePhrase = d.ReasonPhrase; // ReasonPhrases.GetReasonPhrase(ResponseCode); TODO:修复不能json序列化的bug  ReasonPhrases.GetReasonPhrase 是 .netCore 项目的 .net FrameWorker 4.8没有
+                d.Dispose();
+            }
+        }
 
-        public string ResponsePhrase => ReasonPhrases.GetReasonPhrase(ResponseCode);
+        public string ResponsePhrase { get; set; }// => ReasonPhrases.GetReasonPhrase(ResponseCode); TODO:修复不能json序列化的bug
 
         public Header[] ResponseHeaders { get; set; }
 
         public string Body { get; set; }
+
+        private int _responseCode;
     }
 }
