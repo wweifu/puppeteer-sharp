85b65218421c747329d96892ebffe6a46701513d
wweifu <125662872@qq.com>
2021-01-03 14:30:54 +0800

    FetchFulfillRequest 恢复原来的 不做更改

--- lib/PuppeteerSharp/Messaging/FetchFulfillRequest.cs
+++ lib/PuppeteerSharp/Messaging/FetchFulfillRequest.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace PuppeteerSharp.Messaging
@@ -9,24 +7,12 @@ namespace PuppeteerSharp.Messaging
     {
         public string RequestId { get; set; }
 
-        public int ResponseCode
-        {
-            get => _responseCode;
-            set
-            {
-                _responseCode = value;
-                var d = new HttpResponseMessage((HttpStatusCode)_responseCode);
-                ResponsePhrase = d.ReasonPhrase; // ReasonPhrases.GetReasonPhrase(ResponseCode); TODO:修复不能json序列化的bug  ReasonPhrases.GetReasonPhrase 是 .netCore 项目的 .net FrameWorker 4.8没有
-                d.Dispose();
-            }
-        }
+        public int ResponseCode { get; set; }
 
-        public string ResponsePhrase { get; set; }// => ReasonPhrases.GetReasonPhrase(ResponseCode); TODO:修复不能json序列化的bug
+        public string ResponsePhrase => ReasonPhrases.GetReasonPhrase(ResponseCode);
 
         public Header[] ResponseHeaders { get; set; }
 
         public string Body { get; set; }
-
-        private int _responseCode;
     }
 }
